a892d59b33c84fc9fd825f98ae03ef05ee76cab4
Fabiano F. Scalco <fabiano.scalco@gmail.com>
2020-05-10 22:47:20 -0300

    Create controllers for Musics and Artists

--- MyMusic.Api/Startup.cs
+++ MyMusic.Api/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Swagger;
+using AutoMapper;
 
 using MyMusic.Core;
 using MyMusic.Core.Services;
@@ -43,6 +44,8 @@ namespace MyMusic.Api
 
             services.AddSwaggerGen(options =>
                 options.SwaggerDoc("v1", new OpenApiInfo { Title = "My Music", Version = "v1" }));
+
+            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
